8b628bfc89df3992abd42e3c7d7ec1617430353d
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-12 21:23:20 +0300

    Update drivers.controller.ts

--- backend/src/drivers/drivers.controller.ts
+++ backend/src/drivers/drivers.controller.ts
@@ -1,1 +1,50 @@
+import { Controller, Get, Post, Param, Body, Query, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { DriversService } from './drivers.service';
 
+@Controller('drivers')
+export class DriversController {
+  constructor(private readonly driversService: DriversService) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  create(@Body() body: any, @Request() req: any) {
+    return this.driversService.create(body, req.user.id);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  findAll(@Query('status') status?: string) {
+    return this.driversService.findAll(status);
+  }
+
+  @Get('my')
+  @UseGuards(AuthGuard('jwt'))
+  findMy(@Request() req: any) {
+    return this.driversService.findByUser(req.user.id);
+  }
+
+  @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
+  findOne(@Param('id') id: string) {
+    return this.driversService.findOne(id);
+  }
+
+  @Post(':id/approve')
+  @UseGuards(AuthGuard('jwt'))
+  approve(@Param('id') id: string) {
+    return this.driversService.approve(id);
+  }
+
+  @Post(':id/reject')
+  @UseGuards(AuthGuard('jwt'))
+  reject(@Param('id') id: string, @Body() body: { reason: string }) {
+    return this.driversService.reject(id, body.reason);
+  }
+
+  @Post(':id/block')
+  @UseGuards(AuthGuard('jwt'))
+  block(@Param('id') id: string) {
+    return this.driversService.block(id);
+  }
+}
