752c0fe777077a464a21876adc136e4526311f46
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-12 17:02:11 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  app.enableCors({ origin: '*', credentials: true });
+  await app.listen(process.env.PORT ?? 3002, '0.0.0.0');
+}
+bootstrap();
