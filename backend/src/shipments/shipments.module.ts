95545add07bd7d3f5ac0df7c551e8ca1a74464d8
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-12 17:16:41 +0300

    Update shipments.module.ts

--- backend/src/shipments/shipments.module.ts
+++ backend/src/shipments/shipments.module.ts
@@ -1,1 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShipmentsController } from './shipments.controller';
+import { ShipmentsService } from './shipments.service';
+import { Shipment } from './shipment.entity';
+import { Approval } from '../approvals/approval.entity';
+import { Company } from '../companies/company.entity';
+import { Driver } from '../drivers/driver.entity';
 
+@Module({
+  imports: [TypeOrmModule.forFeature([Shipment, Approval, Company, Driver])],
+  controllers: [ShipmentsController],
+  providers: [ShipmentsService],
+  exports: [ShipmentsService],
+})
+export class ShipmentsModule {}
